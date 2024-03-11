e09d7d29b0aa30957af6b679e772265879c155b2
FabianGutierrez05 <125739785+FabianGutierrez05@users.noreply.github.com>
2024-03-11 17:08:51 -0500

    Opciones funcionales
    Ya se hizo que las opciones sean funcionales mediante el html y el js, los array se imprimen de manera correcta con las condiciones

--- js/index.js
+++ js/index.js
@@ -1,12 +1,53 @@
 function numPar(){
+    let numeros = [];
+    for(let i=1;i<=200;i++){
+        numeros.push(i);
+    }
+    const par= numeros.filter(filtroPar);
+    function filtroPar(pares){
+        return pares%2==0;    
+    }
+    console.log(par);
 
 }
 function numImpar(){
-
+    let numeros = [];
+    for(let i=1;i<=200;i++){
+    numeros.push(i);
 }
-function mayPar(){
-
-}
-function menImpar(){
+const par= numeros.filter(filtroImpar);
+function filtroImpar(impares){
+        return impares%2!=0;    
+    }
+console.log(numeros);
     
+}
+
+function mayPar(){
+    const formulario=$('#formulario');
+    const numMay= $('#numMayorPar').val();
+    console.log(numMay);
+    let numeros = [];
+    for(let i=1;i<=200;i++){
+        numeros.push(i);
+    }
+    const par= numeros.filter(filtroPar);
+    function filtroPar(pares){
+        return pares%2==0 && pares>numMay;    
+    }
+    console.log(par);
+}
+function mayImpar(){
+    const formulario=$('#formulario');
+    const numMay= $('#numMayorImpar').val();
+    console.log(numMay);
+    let numeros = [];
+    for(let i=1;i<=200;i++){
+        numeros.push(i);
+    }
+    const par= numeros.filter(filtroImpar);
+    function filtroImpar(pares){
+        return pares%2!=0 && pares>numMay;    
+    }
+    console.log(par);   
 }
