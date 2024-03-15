ee6c187f32d4e1cd82b0675e8c5f323600e8b040
JuanFeMartinez <jfmartinezv@uniboyaca.edu.co>
2024-03-14 23:52:28 -0500

    todas las funciones imprimen y todos los botones funcionan
    se añadio un boton de limpiar para que recargue la pagina, y se imprime todo lo que se ha de imprimir.
    hace falta arreglar impresion en la funcion "todos" y añadir css

--- js/index.js
+++ js/index.js
@@ -4,21 +4,26 @@ let numeros = [];
     }
 
 function todos(){
-    numeros.length;
+    const num= numeros.filter(filtroTodos);
+    function filtroTodos(todos){  
+    }
+    const todfilt = num.map((value, index)=>{
+        return ' El numero en la posicion [' + (index+1)+ '] es: '+ value;
+    })
     console.log(numeros);
+    document.getElementById("impTodos").innerHTML = numeros;
 }
 
 function numPar(){
     const par= numeros.filter(filtroPar);
     function filtroPar(pares){
         return pares%2==0; 
-    ;
     }
     const parfilt = par.map((value, index)=>{
-        return 'El numero par en la posicion [' + (index+1)+ '] es: '+ value;
+        return ' El numero par en la posicion [' + (index+1)+ '] es: '+ value ;
     })
     console.log(parfilt);
-
+    document.getElementById("impPar").innerHTML = parfilt;
 }
 
 function numImpar(){    
@@ -27,10 +32,16 @@ function filtroImpar(impares){
         return impares%2!=0;    
     }
     const imparfilt = impar.map((value, index)=>{
-        return 'El numero impar en la posicion [' + (index+1)+ '] es: '+ value;
+        return ' El numero impar en la posicion [' + (index+1)+ '] es: '+ value;
     })
     console.log(imparfilt);
-    document.getElementById("impPar").innerHTML = imparfilt;
+    document.getElementById("impImpar").innerHTML = imparfilt;
+}
+
+function recargarPagina() {
+    location.reload();
+    var btnRecargar = document.getElementById('btnLimpiar');
+    btnRecargar.addEventListener('click', recargarPagina);
 }
 
 function mayPar(){
@@ -42,16 +53,18 @@ function mayPar(){
         return pares%2==0 && pares>numMay;    
     }
     console.log(par);
+    document.getElementById("impMayPar").innerHTML = par;
 }
 function mayImpar(){
     const formulario=document.forms['formulario'];
-    const numMay= formulario['numMayorImpar'].value;
-    console.log(numMay);
-    const par= numeros.filter(filtroImpar);
-    function filtroImpar(pares){
-        return pares%2!=0 && pares>numMay;    
+    const numMayI= formulario['numMayorImpar'].value;
+    console.log(numMayI);
+    const impar= numeros.filter(filtroImpar);
+    function filtroImpar(impares){
+        return impares%2!=0 && impares>numMayI;    
     }
-    console.log(par);   
+    console.log(impar);   
+    document.getElementById("impMayImp").innerHTML = impar;
 }
 
 /*document.getElementById('tabla').innerHTML='<p>hola</p>'*/
